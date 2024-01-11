2817c226de8c37565561c3f69aef8c00559af799
FiveTW87 <thanwalokwattanakul@gmail.com>
2024-01-11 16:20:28 +0700

    feat:complete Ex

--- exercise-6.js
+++ exercise-6.js
@@ -1,1 +1,10 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: { month: { $month: "$created_at" }, year: { $year: "$created_at" } },
+      total_price_by_month: { $sum: "$total_price" },
+    },
+  },
+  { $match: { "_id.month": 7, "_id.year": 2021 } },
+]);
